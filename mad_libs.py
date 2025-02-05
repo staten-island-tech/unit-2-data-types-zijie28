de064fea0461c2b2690b8f4edeaaa258f10fa290
zijie28 <zijieh@nycstudents.net>
2025-02-05 09:24:30 -0500

    madlibs function

--- mad_libs.py
+++ mad_libs.py
@@ -1,2 +1,11 @@
-a=input("Please type a sentence.")
+""" a=input("Please type a sentence.")
 answer=a.split( )
+print(f"This sentence has {len(answer)} words.") """
+
+l=[]
+d={0:"Verb",1:"Another Verb",2:"Noun",3:"A Number",4:"A Celebrity Guest"}
+def mad_lib():
+    for i in d:
+        a= input(f"Provide a {d[i]}")
+
+mad_lib()
