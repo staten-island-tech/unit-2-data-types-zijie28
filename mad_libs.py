f505163af86c989f94baa5d435fc709466d3eca2
zijie28 <zijieh@nycstudents.net>
2025-02-10 09:25:25 -0500

    Mad libs function and odd or even if statement

--- mad_libs.py
+++ mad_libs.py
@@ -2,14 +2,30 @@
 answer=a.split( )
 print(f"This sentence has {len(answer)} words.") """
 
-l=[]
+""" l=[]
 d={0:"Verb",1:"Another Verb",2:"Another verb", 3:"Noun",4:"A Number",5:"A Celebrity Guest", 6:"A Adjective", 7:"Another Adjective"}
-def mad_lib():
-    for i in d:
-        a= input(f"Provide a {d[i]}")
-        l.append(a)
+n=len(d)
 
-mad_lib()
-        print(f"It was {l[0]} day at school, and {l[5]} was super {l[1]}for lunch. But when she went outside to eat, a {l[3]} stole her food! {l[5]}chased the {l[3]} all over school. She {l[0]}, {l[1]}, and {1[2]} through the playground. Then she tripped on her {l[3]} and the {l[3]} escaped! Luckily, {l[5]}’s friends were willing to share their food with her.")
-        print(len(l))
+a=input("Provide a Verb: ")
+b=input("Provide a Another Verb: ")
+c=input("Provide a Another Verb: ")
+d=input("Provide a Noun: ")
+e=input("Provide a Number: ")
+f=input("Provide a Celebrity Guest: ")
+g=input("Provide a Adjective: ")
+h=input("Provide a Another Adjective: ")
+print(f"It was {g} day at school, and {f} was super {b} for lunch. But when she went outside to eat, a {d} stole her food! {f} chased the {d} all over school. She {a}, {b}, and {c} through the playground. Then she tripped on her {d} and the {d} escaped! Luckily, {f}’s friends were willing to share their food with her.") """
+
+""" x=input("Input a number")
+
+if int(x)%2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd") """
+
+
+    
+
+
+
 
